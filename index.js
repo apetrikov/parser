e0a197f043a40c5c514a56e69a57cc97ffa5f834
apetrikov <jnmort@gmail.com>
2018-04-25 02:49:42 +0300

    Задача 1 решена

--- index.js
+++ index.js
@@ -2,10 +2,43 @@
 // Берёт файл, обрабатывает, выводит результат на порт 3000
 
 const express = require('express');
+const json2html = require('node-json2html');
 const app = express();
 
+const raw1 = require('./src/source1.json');
+const raw2 = require('./src/source2.json');
+const raw3 = require('./src/source3.json');
+const raw41 = require('./src/source4_1.json');
+const raw42 = require('./src/source4_2.json');
+const raw5 = require('./src/source5.json');
+const answers = require('./src/answers');
+
+function task1(arr) {
+  const resArr = [];
+  const h1 = {'<>':'h1','html':'${title}'};
+  const p = {'<>':'p','html':'${body}'};
+  arr.forEach(item => Object.entries(item).map(row => resArr.push({ [row[0]]: row[1] })));
+  const parse = (obj) => {
+    switch (Object.keys(obj)[0]){
+      case 'title':
+        return json2html.transform(JSON.stringify(obj), h1);
+      case 'body':
+        return json2html.transform(JSON.stringify(obj), p);
+    }
+  }
+  return resArr.map(item => parse(item)).join('');
+}
+
+const html1 = task1(raw1);
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  const tests = () => html1 === answers.answer1;
+  res.send(tests());
+});
+
+app.get('/1', function (req, res) {
+  // console.log(html);
+  res.send(html1);
 });
 
 app.listen(3000, function () {
