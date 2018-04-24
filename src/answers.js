0c63fa9072b1587c1dcf05f8ded28a89e29280c8
Andrey Petrikov <apetrikov@rtlservice.com>
2018-04-24 17:48:47 +0300

    Добавил файлы вопросов и ответы

---
+++ src/answers.js
@@ -0,0 +1,8 @@
+// Ответы к задачам
+
+const answer1 = '<h1>Title #1</h1><p>Hello, World 1!</p><h1>Title #2</h1><p>Hello, World 2!</p>';
+const answer2 = '<h3>Title #1</h3><div>Hello, World 1!</div>';
+const answer3 = '<ul><li><h3>Title #1</h3><div>Hello, World 1!</div></li><li><h3>Title #2</h3><div>Hello, World 2!</div></li></ul>';
+const answer41 = '<ul><li><span>Title #1</span><content><ul><li><p>Example 1</p><header>header 1</header></li></ul></content></li><li><div>div 1</div></li></ul>';
+const answer42 = '<p>hello1</p>';
+const answer5 = '<p id="my-id" class="my-class">hello</p><p class="my-class1 my-class2">example&lt;a&gt;asd&lt;/a&gt;</p>';
